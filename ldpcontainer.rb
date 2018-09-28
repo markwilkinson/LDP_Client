ec6cff25051158c56b6b8302841675f2630e02a9
Mark Wilkinson <markw@illuminae.com>
2018-09-28 11:59:10 +0200

    initial

--- ldpcontainer.rb
+++ ldpcontainer.rb
@@ -1,17 +1,7 @@
 require "net/http"
 require "uri"
 
-#uri = URI.parse("http://google.com/")
-
-# Shortcut
-#response = Net::HTTP.get_response(uri)
-
-# Will print response.body
-#Net::HTTP.get_print(uri)
-
-# Full
-#http = Net::HTTP.new(uri.host, uri.port)
-#response = http.request(Net::HTTP::Get.new(uri.request_uri))
+#
 
 class LDPContainer
 
