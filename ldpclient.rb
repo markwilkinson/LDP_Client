64c64b8a350e64d70b4e5dc635871e762aeb7a75
Mark Wilkinson <markw@illuminae.com>
2018-09-28 12:37:40 +0200

    parse LDP message

--- ldpclient.rb
+++ ldpclient.rb
@@ -1,6 +1,7 @@
 require "net/http"
 require "uri"
 require 'openssl'
+require 'rdf/turtle'
 
 #uri = URI.parse("http://google.com/")
 
@@ -37,7 +38,7 @@ class LDPClient
 
   attr_accessor :endpoint
   
-  def initialize (params = {}) # get a name from the "new" call, or set a default
+  def initialize(params = {}) # get a name from the "new" call, or set a default
     @endpoint = params.fetch(:endpoint)
     user = params.fetch(:username)
     pass = params.fetch(:password)
@@ -49,31 +50,39 @@ class LDPClient
       :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
     
       request = Net::HTTP::Get.new uri.request_uri
+      request["User-Agent"] = "My Ruby Script"
+      request["Accept"] = "text/turtle"
+
       request.basic_auth user, pass
     
       response = http.request request # Net::HTTPResponse object
     
-      puts response
-      puts response.body
+      if ["200", "201", "202", "203", "204"].include?(response.code)
+        puts response.body
+        parse_ldp(response.body)
+      else
+        puts "bummer"
+      end
+      
     end
     
   end
 
-  def get (somedisease)
+  def get(somedisease)
     #curl -iX GET -H "Accept: text/turtle"
     #-u dba:fairevaluator
     #"http://evaluations.fairdata.solutions:8890/DAV/home/LDP/evals/"
   end
   
-  def head (somedisease)
+  def head(somedisease)
 
   end
 
-  def put (somedisease)
+  def put(somedisease)
 
   end
 
-  def post (somedisease)
+  def post(somedisease)
     #curl -iX POST -H "Content-Type: text/turtle"
     #-u dav:fairevaluations
     #--data-binary @t.ttl
@@ -82,5 +91,13 @@ class LDPClient
     #"http://evaluations.fairdata.solutions/DAV/home/LDP/evals"
 
   end
+  
+  def parse_ldp(response)
+    repo = RDF::Repository.new   # a repository can be used as a SPARQL endpoint for SPARQL::Client
+    graph = RDF::Graph.new
+    graph.from_ttl(response)
+    puts graph.count
+  end
+  
   
 end
